3099a88c9e29d7fd46753c65fc63c503fbec35be
kavinzhong <68491703+kavinzhong@users.noreply.github.com>
2020-07-19 14:33:54 +0800

    Update C++.cpp
    incresing the code

--- C++.cpp
+++ C++.cpp
@@ -3,3 +3,14 @@ int manin()
 {
   printf(""hello world);
 }
+
+
+class student{
+   student(int a,int b,int c):(m_a)a,(m_b)b,(m_c){};
+  ~student();
+  private:
+  int m_a;
+  int m_b;
+  int m_c;
+
+}
